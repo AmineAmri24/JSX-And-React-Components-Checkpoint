7b0862cd2eed7390eef49d26f19eaeee11523983
AmineAmri24 <amineamri2495@gmail.com>
2023-05-09 12:55:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profilephoto from './Profile/Profilephoto';
+import Fullname from './Profile/Fullname';
+import Adress from './Profile/Adress';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className='welcome'>Hi Gomycode Colleagues</h1>
+      <div className='content'>
+        <div className='writing' >
+          <Fullname/>
+          <Adress/>
+        </div>
+        <Profilephoto/>
+      </div>
     </div>
   );
 }
---
+++ src/Profile/Fullname.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fullname = () => {
+  return (
+    <div>
+      <h1>Amine Amri</h1>
+    </div>
+  )
+}
+
+export default Fullname
